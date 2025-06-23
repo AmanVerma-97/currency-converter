d4095806350df555ff908608b1ed1f184b1ebcaa
Aman Verma <amanksverma@gmail.com>
2025-06-23 22:06:16 +0530

    Change layout color

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -77,9 +77,10 @@ function CurrencyConverter() {
   
   console.log(currencies);
   return (
-    <div className=" max-w-xl mx-auto my-10 p-4 bg-white rounded-md shadow-md shadow-gray-800">
+    
+    <div className=" w-10/12 max-w-xl mx-auto my-10 p-6 bg-white rounded-md shadow-md shadow-rose-600">
 
-        <h2 className=" text-2xl font-semibold text-gray-800 mb-4"> Currency Converter </h2>
+        <h2 className=" text-2xl font-semibold text-gray-800 mb-4 text-center"> Currency Converter </h2>
     
         <div className="mb-4 grid grid-cols-1 sm:grid-cols-3 gap-4 items-end"> 
 
@@ -96,7 +97,7 @@ function CurrencyConverter() {
         </div>
 
         <div> 
-            <label htmlFor="amount" className="block text-sm font-medium text-gray-700 mb-1" > Amount </label>
+            <label htmlFor="amount" className="block text-sm font-medium text-gray-900 mb-1" > Amount </label>
 
             <input 
             type="number" 
@@ -110,7 +111,7 @@ function CurrencyConverter() {
         <div className="flex justify-end mt-5">
             <button 
             onClick={convertCurrency} 
-            className={`px-4 py-2 bg-blue-900 text-white rounded-md hover:bg-blue-950 
+            className={`m-auto px-4 py-2 border border-black font-bold bg-rose-500 text-white rounded-md hover:bg-rose-700 
              focus:outline-none focus:ring-2 focus:ring-blue-700/90 focus:ring-offset-2 ${converting?" animate-pulse":""}`}> Convert</button>
         </div>
 
